27785e8d94a06222698858c51a31aa2d7f58cc01
raniagh <rghzaiel@ggn-group.com>
2024-09-10 15:40:34 +0100

    add shadcn config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
-///import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
+import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
-/* const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
   variable: "--font-sans",
-}); */
+});
 
 export const metadata: Metadata = {
   title: "HealthCare",
@@ -20,7 +22,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`antialiased`}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider attribute='class' defaultTheme='dark'>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
