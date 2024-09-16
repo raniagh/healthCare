4b44c07142059aca25a56276b7201f5355dec43b
raniagh <rghzaiel@ggn-group.com>
2024-09-16 12:52:32 +0100

    add create new user action

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -10,6 +10,7 @@ import SubmitButton from "../SubmitButton";
 import { useState } from "react";
 import { UserFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
+import { createUser } from "@/lib/actions/patient.actions";
 
 export enum FormFieldType {
   INPUT = "input",
@@ -34,33 +35,40 @@ const PatientForm = () => {
     },
   });
 
-  async function onSubmit({
-    name,
-    email,
-    phone,
-  }: z.infer<typeof UserFormValidation>) {
+  const onSubmit = async (values: z.infer<typeof UserFormValidation>) => {
     setIsLoading(true);
 
     try {
-      /*  const userData = { name, email, phone };
-      const user = createUser(userData);
-      if (user) router.push(`/patients/${user.id}/register`); */
-    } catch (error) {}
-  }
+      const user = {
+        name: values.name,
+        email: values.email,
+        phone: values.phone,
+      };
+      const newUser = await createUser(user);
+
+      if (newUser) {
+        router.push(`/patients/${newUser.$id}/register`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6 flex-1'>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='flex-1 space-y-6'>
         <section className='mb-12 space-y-4'>
           <h1 className='header'>Hi there 👋</h1>
           <p className='text-dark-700'>Get started with appointments.</p>
         </section>
+
         <CustomFormField
           fieldType={FormFieldType.INPUT}
           control={form.control}
           name='name'
           label='Full name'
-          placeholder='Full name'
+          placeholder='John Doe'
           iconSrc='/assets/icons/user.svg'
           iconAlt='user'
         />
@@ -70,7 +78,7 @@ const PatientForm = () => {
           control={form.control}
           name='email'
           label='Email'
-          placeholder='example@gmail.com'
+          placeholder='johndoe@gmail.com'
           iconSrc='/assets/icons/email.svg'
           iconAlt='email'
         />
@@ -78,9 +86,9 @@ const PatientForm = () => {
         <CustomFormField
           fieldType={FormFieldType.PHONE_INPUT}
           control={form.control}
-          name='PHONE'
+          name='phone'
           label='Phone number'
-          placeholder='(333) 123-52445'
+          placeholder='(555) 123-4567'
         />
 
         <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
