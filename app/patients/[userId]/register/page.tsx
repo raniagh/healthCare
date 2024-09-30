483bc2c4689ffd2c975b328e269f94444ee2b314
raniagh <rghzaiel@ggn-group.com>
2024-09-30 11:12:00 +0100

    add sentry for security

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -2,9 +2,12 @@ import RegisterForm from "@/components/forms/RegisterForm";
 import { getUser } from "@/lib/actions/patient.actions";
 import Image from "next/image";
 import React from "react";
+import * as Sentry from "@sentry/nextjs";
 
 const Register = async ({ params: { userId } }: SearchParamProps) => {
   const user = await getUser(userId);
+
+  Sentry.metrics.set("user_view_registration", user.name);
 
   return (
     <div className='flex h-screen max-h-screen'>
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://176b8f661b26b5a0bcf60ba41ee8bbb8@o4508040757903360.ingest.de.sentry.io/4508040782413904",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
